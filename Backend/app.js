5ae7a9fad3c249063ab503f20862f79a4be9248f
Srishti Verma <srishtiverma358@gmail.com>
2025-09-02 00:02:03 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,21 @@
+const dotenv = require("dotenv")
+dotenv.config();
 const express = require("express")
+const cors = require('cors');
+const connectToDb = require('./db/db')
+const userRoutes = require('./routes/user.routes')
 const app = express()
+
+connectToDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/users', userRoutes)
 
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(port, () => {
-    `Server has started at port ${port}`
-})
+module.exports = app;
